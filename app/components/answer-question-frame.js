488d5ed977474a0f896f3acee45114c46a91feea
kyle <altck8@gmail.com>
2016-02-26 12:56:52 -0800

    html styling on question page

--- app/components/answer-question-frame.js
+++ app/components/answer-question-frame.js
@@ -8,13 +8,20 @@ export default Ember.Component.extend({
       this.set("showAnswerArea", true);
       this.set("showAnswerButton", false);
     },
+    closeAnswerArea( ) {
+      this.set("answer", ""),
+      this.set("content", ""),
+      this.set("showAnswerArea", false);
+      this.set("showAnswerButton", true);
+    },
     submitAnswer( ) {
       var params = {
         content: this.get("answer"),
-        author: this.get("author"),
+        author: this.get("content"),
       };
+      this.sendAction('submitAnswer', params);
       this.set("showAnswerArea", false);
       this.set("showAnswerButton", true);
-    }
+    },
   }
 });
