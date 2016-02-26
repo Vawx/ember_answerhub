6239f7e97f3dd8548b07680e02e6cf94c5b94587
kyle <altck8@gmail.com>
2016-02-26 11:47:44 -0800

    add question frame component

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'answerhub',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://answerboard.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
